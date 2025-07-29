3f14177f5dc79eb8b351ee4f77a49a4a69a77a55
Yansheng Miao <sinordamiao@gmail.com>
2025-07-29 18:34:50 -0400

    Create NavBar+Pk gamemap and walls

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,34 +1,15 @@
 <template>
-  <div>
-    <div>bot name: {{ bot_name }}</div>
-    <div>bot rating: {{ bot_rating }}</div>
-  </div>
+  <NavBar />
   <router-view/>
 </template>
 
 <script> //js
-import $ from 'jquery';
-import { ref } from 'vue';
-
+import NavBar from '@/components/NavBar.vue'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap"
 export default {
-  name: "App",
-  setup: () => {
-      let bot_name = ref("");
-      let bot_rating = ref(""); 
-      $.ajax({
-        url: "http://127.0.0.1:3000/pk/getbotinfo/",
-        type: "GET",
-        success: resp => {
-          //console.log(resp);
-          bot_name.value = resp.name;
-          bot_rating.value = resp.rating;
-        }
-      });
-
-      return {
-        bot_name,
-        bot_rating,
-      }
+  components: {
+    NavBar
   }
 }
 </script>
@@ -36,7 +17,7 @@ export default {
 <style> /*css */
 body {
   /* @: current root directory */
-  background-image: url("@/assets/logo.png");
+  background-image: url("@/assets/images/logo.png");
   background-size: cover;
 }
 </style>
---
+++ web/src/views/pk/PkIndexView.vue
@@ -0,0 +1,23 @@
+<template>
+
+    <PlayGround />
+
+</template>
+
+<script>
+import PlayGround from '@/components/PlayGround.vue'
+export default {
+    components: {
+        PlayGround
+    },
+}
+
+</script>
+
+<style scoped>
+div.playground {
+    width: 60vw;
+    height: 70vh;
+    margin: 40px auto;
+}
+</style>
